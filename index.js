c7738b53b4f0fc270729c76c8d46fae6d39cee1f
Curtis Moses <mixedbymoses@gmail.com>
2023-06-26 14:12:01 -0700

    All State Events Complete

--- index.js
+++ index.js
@@ -12,15 +12,21 @@ const newJobButton = document.getElementById("new-job-btn");
 const newJobForm = document.querySelector("#new-job-form");
 const backPartBtn = document.querySelector("#back-part-btn");
 const backJobBtn = document.querySelector("#back-job-btn");
+const jobImgIcon = document.querySelector("#job-image-icon");
+const detailState = document.querySelector("#detail-state");
 
 //FETCH FUNCTIONS
 
 //EVENT LISTENERS
 
+//Detail State Toggle
+jobImgIcon.addEventListener("click", toggleDetailState);
+
 //Create New Part Button State Toggle
 newPartButton.addEventListener("click", toggleNewPartState);
 newJobButton.addEventListener("click", toggleNewJobState);
 
+//Back Button Toggle
 backPartBtn.addEventListener("click", (e) => {
   e.preventDefault();
   backToHomeState();
@@ -49,6 +55,16 @@ urlButton.addEventListener("click", function () {
 dropArea.addEventListener("drop", handleDrop, false);
 
 //EVENT HANDLERS
+
+//For Detail State Toggle
+function toggleDetailState(e) {
+  e.preventDefault();
+  if (detailState.classList.contains("hidden")) {
+    detailState.classList.remove("hidden");
+  } else {
+    detailState.classList.add("hidden");
+  }
+}
 
 //For Back Button State Toggle
 function backToHomeState() {
