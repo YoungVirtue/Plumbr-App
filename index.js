6ce3b9f8c34d1460d6e364484446768391bbcf6f
Curtis Moses <mixedbymoses@gmail.com>
2023-06-29 15:34:23 -0700

    moved delete to HTML, removed jobs from db.json

--- index.js
+++ index.js
@@ -22,7 +22,8 @@ const partDetailCard = document.querySelector("#part-detail-card");
 const imageDetail = document.querySelector("#image-detail");
 const partCardName = document.querySelector("#part-detail-name");
 const editDeleteButtons = document.querySelector("#edit-delete-buttons");
-let selectedJobs;
+const deleteButton = document.querySelector(".delete-button");
+// let selectedJobs;
 
 //FETCH FUNCTIONS
 
@@ -130,184 +131,236 @@ function handleJobSubmit(e) {
       return res.json();
     })
     .then((newJobJson) => {
+      const storedJobIds = getStoredJobIds();
+      storedJobIds.push(newJobJson.id);
+      localStorage.setItem("jobIds", JSON.stringify(storedJobIds));
       // Save the ID to localStorage
-      localStorage.setItem("jobId", newJobJson.id);
       // Set the ID as the image element's attribute
-      const imageElement = document.getElementById("myImage");
-      imageElement.setAttribute("data-job-id", newJobJson.id);
+      // const imageElement = document.getElementById("myImage");
+      image.setAttribute("data-job-id", newJobJson.id);
     });
+
+    updateImagesWithJobIds();
+
+}
+
+//Handle Parts Dropdown
+function handlePartsDropdown() {
+  if (partsList.classList.contains("hidden")) {
+    partsList.classList.remove("hidden");
+  } else {
+    partsList.classList.add("hidden");
   }
+}
 
-  //Handle Parts Dropdown
-  function handlePartsDropdown() {
-    if (partsList.classList.contains("hidden")) {
-      partsList.classList.remove("hidden");
-    } else {
-      partsList.classList.add("hidden");
-    }
+//For Detail State Toggle
+function toggleDetailState(e) {
+  if (detailState.classList.contains("hidden")) {
+    detailState.classList.remove("hidden");
+  } else if (e.target.src === imageDetail.src) {
+    detailState.classList.add("hidden");
   }
+}
 
-  //For Detail State Toggle
-  function toggleDetailState(e) {
-    if (detailState.classList.contains("hidden")) {
-      detailState.classList.remove("hidden");
-    } else if (e.target.src === imageDetail.src) {
-      detailState.classList.add("hidden");
-    }
+//For Part Detail Toggle
+
+function togglePartCard(e) {
+  if (partDetailCard.classList.contains("hidden")) {
+    partDetailCard.classList.remove("hidden");
+  } else if (e.target.textContent === partCardName.textContent) {
+    partDetailCard.classList.add("hidden");
   }
+}
 
-  //For Part Detail Toggle
-
-  function togglePartCard(e) {
-    if (partDetailCard.classList.contains("hidden")) {
-      partDetailCard.classList.remove("hidden");
-    } else if (e.target.textContent === partCardName.textContent) {
-      partDetailCard.classList.add("hidden");
-    }
+//For New Part Button State Toggle
+function toggleNewPartState() {
+  if (newPartForm.classList.contains("hidden")) {
+    newPartForm.classList.remove("hidden");
+    newPartButton.textContent = "Done";
+  } else {
+    newPartForm.classList.add("hidden");
+    newPartButton.textContent = "Create New Part";
   }
+}
 
-  //For New Part Button State Toggle
-  function toggleNewPartState() {
-    if (newPartForm.classList.contains("hidden")) {
-      newPartForm.classList.remove("hidden");
-      newPartButton.textContent = "Done";
-    } else {
-      newPartForm.classList.add("hidden");
-      newPartButton.textContent = "Create New Part";
-    }
+//For New Job Button State Toggle
+function toggleNewJobState() {
+  if (newJobForm.classList.contains("hidden")) {
+    newJobForm.classList.remove("hidden");
+    newJobButton.textContent = "Done";
+  } else {
+    newJobForm.classList.add("hidden");
+    newJobButton.textContent = "Create New Job";
   }
+}
 
-  //For New Job Button State Toggle
-  function toggleNewJobState() {
-    if (newJobForm.classList.contains("hidden")) {
-      newJobForm.classList.remove("hidden");
-      newJobButton.textContent = "Done";
-    } else {
-      newJobForm.classList.add("hidden");
-      newJobButton.textContent = "Create New Job";
-    }
-  }
+//For Delete Job Button
+function deleteJob(job) {
+  fetch(`${jobURL}/${job.id}`),
+    {
+      method: "DELETE",
+    };
+  console.log(job);
+}
 
-  //For Delete Job Button
-  function deleteJob(job) {
-    fetch(`${jobURL}/${job.id}`),
-      {
-        method: "DELETE",
-      };
-    console.log(job);
-  }
+//RENDER FUNCTIONS
 
-  //RENDER FUNCTIONS
+//Render Existing Jobs in Home State Icons
 
-  //Render Existing Jobs in Home State Icons
+function renderIconsHomeState(jobsObj) {
+  const jobIconImg = document.createElement("img");
+  jobIconImg.src = jobsObj.image;
+  jobIconImg.classList.add("rounded-5", "p-2");
+  jobIcons.appendChild(jobIconImg);
 
-  function renderIconsHomeState(jobsObj) {
-    const jobIconImg = document.createElement("img");
-    jobIconImg.src = jobsObj.image;
-    jobIconImg.classList.add("rounded-5", "p-2");
-    jobIcons.appendChild(jobIconImg);
+  jobIconImg.addEventListener("click", (e) => {
+    toggleDetailState(e);
+    renderDetailState(jobsObj);
+  });
+}
 
-    jobIconImg.addEventListener("click", (e) => {
-      toggleDetailState(e);
-      renderDetailState(jobsObj);
-    });
-  }
+//Display Detail in Job Detail State
 
-  //Display Detail in Job Detail State
+function renderDetailState(newJob) {
+  const detailImg = document.querySelector("#image-detail");
+  const detailName = document.querySelector("#job-detail-name");
+  const detailDescr = document.querySelector("#job-description-name");
+  const ul = document.querySelector("#job-parts-detail-list");
+  // const deleteButton = document.createElement("button");
+  const editButton = document.createElement("button");
+  editButton.classList.add("edit-button", "my-2");
+  editButton.textContent = "Edit Job";
+  // deleteButton.className = "delete-button";
+  // deleteButton.textContent = "Delete Job";
+  editDeleteButtons.innerHTML = "";
+  editDeleteButtons.appendChild(editButton);
+  editDeleteButtons.appendChild(deleteButton);
 
-  function renderDetailState(newJob) {
-    const detailImg = document.querySelector("#image-detail");
-    const detailName = document.querySelector("#job-detail-name");
-    const detailDescr = document.querySelector("#job-description-name");
-    const ul = document.querySelector("#job-parts-detail-list");
-    const deleteButton = document.createElement("button");
-    const editButton = document.createElement("button");
-    editButton.classList.add("edit-button", "my-2");
-    editButton.textContent = "Edit Job";
-    deleteButton.className = "delete-button";
-    deleteButton.textContent = "Delete Job";
-    editDeleteButtons.innerHTML = "";
-    editDeleteButtons.appendChild(editButton);
-    editDeleteButtons.appendChild(deleteButton);
+  // deleteButton.addEventListener("click", deleteJob);
 
-    deleteButton.addEventListener("click", deleteJob);
+  const jobPartsArr = newJob.parts;
 
-    const jobPartsArr = newJob.parts;
+  detailImg.src = newJob.image;
+  detailName.textContent = newJob.name;
+  detailDescr.textContent = newJob.description;
+  ul.innerHTML = "";
 
-    detailImg.src = newJob.image;
-    detailName.textContent = newJob.name;
-    detailDescr.textContent = newJob.description;
-    ul.innerHTML = "";
-
-    jobPartsArr.forEach((part) => {
-      getParts(partURL + "/" + part).then((part) => {
-        let li = document.createElement("li");
-        li.textContent = part.name;
-        ul.appendChild(li);
-        li.addEventListener("click", (e) => {
-          togglePartCard(e);
-          renderPartsDetail(part);
-        });
+  jobPartsArr.forEach((part) => {
+    getParts(partURL + "/" + part).then((part) => {
+      let li = document.createElement("li");
+      li.textContent = part.name;
+      ul.appendChild(li);
+      li.addEventListener("click", (e) => {
+        togglePartCard(e);
+        renderPartsDetail(part);
       });
     });
-  }
-
-  function renderPartsDetail(part) {
-    const partDetailImage = document.querySelector("#part-detail-img");
-    const partDetailName = document.querySelector("#part-detail-name");
-    const partDetailSize = document.querySelector("#part-detail-size");
-    const partDetailType = document.querySelector("#part-detail-type");
-    const partDetailTags = document.querySelector("#part-detail-tags");
-    partDetailTags.innerHTML = "";
-    const tagArr = part.tags;
-
-    partDetailImage.src = part.image;
-    //partDetailImage.id = part.id;
-    partDetailName.textContent = part.name;
-    partDetailSize.textContent = `Size: ${part.size}`;
-    partDetailType.textContent = `Type: ${part.type}`;
-    tagArr.forEach((tag) => {
-      let li = document.createElement("li");
-      li.textContent = `#${tag}`;
-      li.classList.add("list-group-item");
-      partDetailTags.appendChild(li);
-    });
-  }
-
-  //Display Parts Data in Parts List
-  function renderInPartsList(partObj) {
-    const formCheck = document.createElement("label");
-    const input = document.createElement("input");
-    const label = document.createElement("span");
-    formCheck.classList.add("label");
-    input.type = "checkbox";
-    input.id = partObj.id;
-    input.name = partObj.name;
-    input.value = partObj.id;
-    input.classList.add("nes-checkbox");
-    label.htmlFor = partObj.id;
-    label.textContent = partObj.name;
-    // label.classList.add("label")
-    partsList.appendChild(formCheck);
-    formCheck.appendChild(input);
-    formCheck.appendChild(label);
-  }
-
-  // Display uploaded file
-  function displayUploadedFile(file) {
-    var reader = new FileReader();
-    reader.onload = function (e) {
-      displayImage.src = e.target.result;
-    };
-    reader.readAsDataURL(file);
-  }
-
-  //INITIALIZERS
-  getParts(partURL).then((partsArr) => {
-    partsArr.forEach(renderInPartsList);
   });
+}
 
-  getJobs(jobURL).then((jobsArr) => {
-    jobsArr.forEach(renderIconsHomeState);
+function renderPartsDetail(part) {
+  const partDetailImage = document.querySelector("#part-detail-img");
+  const partDetailName = document.querySelector("#part-detail-name");
+  const partDetailSize = document.querySelector("#part-detail-size");
+  const partDetailType = document.querySelector("#part-detail-type");
+  const partDetailTags = document.querySelector("#part-detail-tags");
+  partDetailTags.innerHTML = "";
+  const tagArr = part.tags;
+
+  partDetailImage.src = part.image;
+  //partDetailImage.id = part.id;
+  partDetailName.textContent = part.name;
+  partDetailSize.textContent = `Size: ${part.size}`;
+  partDetailType.textContent = `Type: ${part.type}`;
+  tagArr.forEach((tag) => {
+    let li = document.createElement("li");
+    li.textContent = `#${tag}`;
+    li.classList.add("list-group-item");
+    partDetailTags.appendChild(li);
   });
+}
 
+//Display Parts Data in Parts List
+function renderInPartsList(partObj) {
+  const formCheck = document.createElement("label");
+  const input = document.createElement("input");
+  const label = document.createElement("span");
+  formCheck.classList.add("label");
+  input.type = "checkbox";
+  input.id = partObj.id;
+  input.name = partObj.name;
+  input.value = partObj.id;
+  input.classList.add("nes-checkbox");
+  label.htmlFor = partObj.id;
+  label.textContent = partObj.name;
+  // label.classList.add("label")
+  partsList.appendChild(formCheck);
+  formCheck.appendChild(input);
+  formCheck.appendChild(label);
+}
+
+// Display uploaded file
+function displayUploadedFile(file) {
+  var reader = new FileReader();
+  reader.onload = function (e) {
+    displayImage.src = e.target.result;
+  };
+  reader.readAsDataURL(file);
+}
+
+//INITIALIZERS
+getParts(partURL).then((partsArr) => {
+  partsArr.forEach(renderInPartsList);
+});
+
+getJobs(jobURL).then((jobsArr) => {
+  jobsArr.forEach(renderIconsHomeState);
+});
+
+// ...
+
+// Function to retrieve the job ID from localStorage
+// Function to retrieve all stored job IDs from localStorage
+function getStoredJobIds() {
+  const jobIdsString = localStorage.getItem("jobIds");
+  if (jobIdsString) {
+    return JSON.parse(jobIdsString);
+  } else {
+    return [];
+  }
+}
+
+// Example usage:
+const storedJobIds = getStoredJobIds();
+
+// Function to update the image element with the stored job ID
+// Function to update all image elements with their respective job IDs
+function updateImagesWithJobIds() {
+  const imageElements = jobIcons.querySelectorAll("img");
+  const storedJobIds = getStoredJobIds();
+  imageElements.forEach((imageElement) => {
+    const jobId = imageElement.getAttribute("data-job-id");
+    if (jobId && storedJobIds.includes(jobId)) {
+      imageElement.setAttribute("data-job-id", jobId);
+    }
+  });
+}
+
+
+// Run the function when the page loads
+window.addEventListener("load", updateImagesWithJobIds);
+updateImagesWithJobIds();
+
+
+//   deleteButton.addEventListener('click', () => {
+//     const jobItem = jobIcons.querySelectorAll('img')
+//     jobItem.forEach((job) => {
+//         if (job.id === selectedJobs.name) {
+//             job.remove()
+//             fetch(`${jobURL}/${selectedJobs.id}`, {
+//                 method: "DELETE"
+//             })
+//             .then(res => res.json())
+//         }
+//     })
+// })
---
+++ readme.md
@@ -0,0 +1,58 @@
+# PLUMBR
+
+PLUMBR is a web application that allows users to manage plumbing parts and create jobs using those parts. It provides a user-friendly interface for adding parts to a database and creating new jobs that include specific parts from the database. Users can click on job icons to view detailed information about a specific job, and within the job detail view, they can click on parts to see detailed information about each part. The application also allows users to delete jobs from the database within the job detail view. A search bar is provided to search for jobs, and the search results are rendered in the detail view.
+
+## Installation
+
+No installation is required to use the PLUMBR web application. Simply open the application in a web browser to get started.
+
+## Usage
+
+1. Add Parts:
+   - Click on the "Create New Part" button to open the new part form.
+   - Fill in the required fields (name, size, type) and any additional tags or an image URL for the part.
+   - Click the "Submit" button to add the part to the database.
+
+2. Create Jobs:
+   - Click on the "Create New Job" button to open the new job form.
+   - Enter a name and description for the job.
+   - Select the required parts for the job from the provided list.
+   - Click the "Submit" button to create the job.
+
+3. View Job Details:
+   - On the home screen, job icons representing existing jobs are displayed.
+   - Click on a job icon to view detailed information about the job.
+   - The job detail view shows the job's image, name, description, and the parts associated with the job.
+   - Clicking on a part in the job detail view will display detailed information about that part.
+
+4. Delete Jobs:
+   - Within the job detail view, click the "Delete Job" button to delete the job from the database.
+
+5. Search Jobs:
+   - Use the search bar to search for jobs.
+   - As you type, the application filters the jobs based on the entered text.
+   - The filtered jobs are rendered in the detail view.
+
+## Future Features
+
+The PLUMBR application has plans for future enhancements and features, including:
+
+- Edit button functionality to allow users to modify existing jobs and parts.
+- User profiles to enable personalized experiences and data management.
+- Integration with a real database system for more robust and scalable data storage.
+- Ability to choose from a set list of icons for the new job image to provide visual customization options.
+- Enhanced search capabilities, including searching by specific parts and adding them to jobs directly from the search dropdown.
+
+## Support
+
+For support or assistance with PLUMBR, you can reach out to our support team at [support@plumbr.com](mailto:support@plumbr.com).
+
+
+## Authors and Acknowledgment
+
+PLUMBR is developed and maintained by the PLUMBR development team - Christina Lambiase & Curtis Moses
+
+
+## Project Status
+
+Development of PLUMBR is active and ongoing, with future updates and enhancements planned.
